2c4ff96094d07e06016daf441593c93706ef0d00
kartikDas074 <kartikdas209774@gmail.com>
2026-06-26 01:59:08 +0600

    Adding verify functionality

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 dotenv.config();
 
 const app = express();
@@ -15,6 +18,90 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const DB = await client.db("BloodNet");
+    const DonationRequest = DB.collection("DonationRequest");
+    const Session = DB.collection("session");
+    const user = DB.collection("user");
+
+    const VerifyToken = async (req, res, next) => {
+      try {
+        const authHeader = req.headers?.authorization;
+
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
+          return res.status(401).json({
+            success: false,
+            message: "Unauthorized access",
+          });
+        }
+
+        const token = authHeader.split(" ")[1];
+
+        if (!token) {
+          return res.status(401).json({
+            success: false,
+            message: "Unauthorized access",
+          });
+        }
+
+        const session = await Session.findOne({ token });
+
+        if (!session) {
+          return res.status(403).json({
+            success: false,
+            message: "Forbidden access",
+          });
+        }
+
+        const user = await userCollection.findOne({
+          _id: new ObjectId(session.userId),
+        });
+
+        if (!user) {
+          return res.status(404).json({
+            success: false,
+            message: "User not found",
+          });
+        }
+
+        req.user = user;
+
+        next();
+      } catch (error) {
+        console.error("Error in verifyToken:", error);
+
+        return res.status(500).json({
+          success: false,
+          message: "Internal server error",
+        });
+      }
+    };
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
